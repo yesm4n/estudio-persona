6645bad12d82f346ff866840ac8ebae573bf5910
yesm4n <mrylliramadani@gmail.com>
2023-10-10 12:25:21 +0200

    fixed headers

--- src/components/StudioPage.jsx
+++ src/components/StudioPage.jsx
@@ -3,6 +3,7 @@ import styles from "./StudioPage.module.css";
 function StudioPage() {
   return (
     <main>
+      {/* Section one */}
       <section className={styles.one}>
         <div>
           <img
@@ -27,24 +28,23 @@ function StudioPage() {
           />
         </div>
       </section>
+      {/* Section one */}
 
-      {/* BUG */}
-      {/* BUG */}
-      {/* BUG */}
-      {/* BUG */}
-      {/* BUG */}
+      {/* Section two */}
       <section className={styles.two}>
         <h1 className={styles.h2}>
-          <span>Estudio Persona was created</span>
-          <span>
-            by Emiliana Gonzalez and Jessie Young in 2015. Born in Uruguay, they
-            both met in Los Angeles where they started working on a
-            collaborative process that evolved into the formation of Estudio
-            Persona.
-          </span>
+          <span className={styles.spacing}>
+            {" "}
+            Estudio Persona was created by
+          </span>{" "}
+          Emiliana Gonzalez and Jessie Young in 2015. Born in Uruguay, they both
+          met in Los Angeles where they started working on a collaborative
+          process that evolved into the formation of Estudio Persona.
         </h1>
       </section>
+      {/* Section two */}
 
+      {/* Section three */}
       <section className={styles.three}>
         <span className={styles.explore}>Explore Collections</span>
         <div className={styles.threeContent}>
@@ -99,7 +99,9 @@ function StudioPage() {
           />
         </div>
       </section>
+      {/* Section three */}
 
+      {/* Section four */}
       <section className={styles.four}>
         <img
           src="images/studio/6.png"
@@ -114,23 +116,19 @@ function StudioPage() {
           draggable="false"
         />
       </section>
+      {/* Section four */}
 
+      {/* Section five */}
       <section className={styles.five}>
-        {/* BUG */}
-        {/* BUG */}
-        {/* BUG */}
-        {/* BUG */}
-        {/* BUG */}
-        <h2 className={styles.h2}>
-          <span>At the core of their practice</span>
-          <span>
-            lies a desire to unearth the full potential of raw materials and a
-            creative expression reflective of their cultural identity and
-            heritage.
-          </span>
-        </h2>
+        <h3 className={styles.h3}>
+          At the core of their practice lies a desire to unearth the full
+          potential of raw materials and a creative expression reflective of
+          their cultural identity and heritage.
+        </h3>
       </section>
+      {/* Section five */}
 
+      {/* Section six */}
       <section className={styles.six}>
         <p>
           Estudio Persona was created by Emiliana Gonzalez and Jessie Young in
@@ -141,7 +139,9 @@ function StudioPage() {
           sculptural lines.
         </p>
       </section>
+      {/* Section six */}
 
+      {/* Section seven */}
       <section className={styles.seven}>
         <img
           className={styles.imgEight}
@@ -186,6 +186,7 @@ function StudioPage() {
           draggable="false"
         />
       </section>
+      {/* Section seven */}
     </main>
   );
 }
