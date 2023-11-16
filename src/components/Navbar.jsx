646e3a3a33175b0e778b089959473203a1dee37e
yesm4n <mrylliramadani@gmail.com>
2023-11-16 06:38:43 +0100

    burger menu added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,46 @@
-import { NavLink } from "react-router-dom";
-import styles from "./Navbar.module.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-function Navbar() {
+const Navbar = () => {
+  const [open, setOpen] = useState(true);
+
+  const handleClick = () => {
+    setOpen((open) => !open);
+  };
+
   return (
-    <nav className={styles.nav}>
-      <span>
-        <NavLink to="/">Estudio Persona</NavLink>
-      </span>
+    <header>
+      {open ? (
+        <img
+          src="images/menu.png"
+          className={open ? "open" : "close"}
+          onClick={handleClick}
+        />
+      ) : (
+        <img
+          src="images/close.png"
+          className={open ? "open" : "close"}
+          onClick={handleClick}
+        />
+      )}
+      <nav className={!open ? "active" : ""}>
+        <span>
+          <Link to="/">Estudio Persona</Link>
+        </span>
 
-      <ul>
-        <li>
-          <NavLink to="/studio">Studio</NavLink>
-        </li>
-        <li className={styles.disabled}>Shop</li>
-        <li>
-          <NavLink to="/index">Index</NavLink>
-        </li>
-        <li className={styles.disabled}>Journal</li>
-      </ul>
-    </nav>
+        <ul>
+          <li>
+            <Link to="/studio">Studio</Link>
+          </li>
+          <li className="disabled">Shop</li>
+          <li>
+            <Link to="/index">Index</Link>
+          </li>
+          <li className="disabled">Journal</li>
+        </ul>
+      </nav>
+    </header>
   );
-}
+};
 
 export default Navbar;
