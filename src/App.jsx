1e349b34d81dca1ee2d4d30726c7b1008f06a887
yesm4n <mrylliramadani@gmail.com>
2024-06-08 04:17:00 +0200

    lenis bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,27 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ReactLenis } from 'lenis/react';
 
-import Navbar from "./components/Navbar";
-import Container from "./components/Container";
-import Homepage from "./pages/Homepage";
-import Index from "./pages/Index";
-import Studio from "./pages/Studio";
-import PageNotFound from "./pages/PageNotFound";
+import Navbar from './components/Navbar';
+import Container from './components/Container';
+import Homepage from './pages/Homepage';
+import Index from './pages/Index';
+import Studio from './pages/Studio';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
-  useEffect(() => {
-    const lenis = new Lenis();
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  }, []);
-
   return (
     <BrowserRouter>
       <Navbar />
-      <Container>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="/studio" element={<Studio />} />
-          <Route path="/index" element={<Index />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Container>
+      <ReactLenis root>
+        <Container>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="/studio" element={<Studio />} />
+            <Route path="/index" element={<Index />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Container>
+      </ReactLenis>
     </BrowserRouter>
   );
 }
